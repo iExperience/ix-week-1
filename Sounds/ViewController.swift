3f76e3b8156339644efaac9cb8a80b95dc5929a3
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2017-05-25 16:24:11 +0200

    Initial Commit

---
+++ Sounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sounds
+//
+//  Created by Miki von Ketelhodt on 2017/05/25.
+//  Copyright © 2017 RBG Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
