cf37496d5de071ee8e7b8c810853653ff9a1d730
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2017-05-26 20:20:55 +0200

    Rename ViewController to RecordViewController and change storyboard view properties to point to correct controller class

--- Sounds/RecordViewController.swift
+++ Sounds/RecordViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class RecordViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
